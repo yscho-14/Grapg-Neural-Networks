990d69061b1cc7cd0122341ebb557fc148b81873
yeonseokcho <ikgastph@gmail.com>
2026-02-16 08:04:59 +0900

    Update GAT.py
    GAT 코드

--- GAT.py
+++ GAT.py
@@ -37,3 +37,28 @@ if torch.cuda.is_available():
     torch.backends.cudnn.benchmark = False
 
 print("Installation Complete.")
+
+# GAT, Graph Attention Networks
+
+# 1. library
+import torch
+
+find_links = f"https://data.pyg.org/whl/torch-{torch.__version__}.html"
+
+!pip install -q \
+    torch-scatter \
+    torch-sparse \
+    torch-cluster \
+    torch-spline-conv \
+    torch-geometric \
+    -f $find_links
+
+# seed set
+torch.manual_seed(0)
+if torch.cuda.is_available():
+    torch.cuda.manual_seed(0)
+    torch.cuda.manual_seed_all(0)
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = False
+
+print("Installation Complete.")
