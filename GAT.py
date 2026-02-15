4aa3503b5d30c08881e7750d83bb958ea6b96a59
yeonseokcho <ikgastph@gmail.com>
2026-02-16 07:16:57 +0900

    Update GAT.py

--- GAT.py
+++ GAT.py
@@ -1,1 +1,39 @@
+import numpy as np # linear algebra
+import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
 
+import os
+for dirname, _, filenames in os.walk('/kaggle/input'):
+    for filename in filenames:
+        print(os.path.join(dirname, filename))
+        
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+import warnings
+warnings.filterwarnings("ignore", category=Warning)
+
+
+# GAT, Graph Attention Networks
+
+# 1. library
+import torch
+
+find_links = f"https://data.pyg.org/whl/torch-{torch.__version__}.html"
+
+!pip install -q \
+    torch-scatter \
+    torch-sparse \
+    torch-cluster \
+    torch-spline-conv \
+    torch-geometric \
+    -f $find_links
+
+# seed set
+torch.manual_seed(0)
+if torch.cuda.is_available():
+    torch.cuda.manual_seed(0)
+    torch.cuda.manual_seed_all(0)
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = False
+
+print("Installation Complete.")
